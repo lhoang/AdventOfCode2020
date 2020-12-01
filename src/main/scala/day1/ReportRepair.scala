6b0681e8d75c871dc59d07d3056b1827b430658c
lang <hoanglang@gmail.com>
2020-12-01 11:23:00 +0100

    Day 1

--- src/main/scala/day1/ReportRepair.scala
+++ src/main/scala/day1/ReportRepair.scala
@@ -1,5 +1,16 @@
 package day1
 
 class ReportRepair {
+  def find2Entries(inputs: List[String]): Long =
+    findEntries(inputs, 2)
 
+  def find3Entries(inputs: List[String]): Long =
+    findEntries(inputs, 3)
+
+  def findEntries(inputs: List[String], n: Int): Long =
+    inputs.map(_.toLong)
+      .combinations(n)
+      .find(_.sum == 2020)
+      .map(_.product)
+      .getOrElse(0L)
 }
--- src/test/scala/day1/ReportRepairTest.scala
+++ src/test/scala/day1/ReportRepairTest.scala
@@ -1,7 +1,33 @@
 package day1
 
 import utils.BaseTestSuite
+import utils.InputUtils.read
 
 class ReportRepairTest extends BaseTestSuite{
+
+  val sut = new ReportRepair
+
+  "Report Repair" should "find 2 entries  == 2020 (example)" in {
+    val inputs = List("1721", "979", "366", "299", "675", "1456")
+    val res = sut.find2Entries(inputs)
+    res shouldBe 514579
+  }
+
+  it should "give the answer to the challenge - Part 1" in {
+    val res = sut.find2Entries(read("day1.txt"))
+    res shouldBe 926464
+  }
+
+
+  it should "find 3 entries  == 2020 (example)" in {
+    val inputs = List("1721", "979", "366", "299", "675", "1456")
+    val res = sut.find3Entries(inputs)
+    res shouldBe 241861950
+  }
+
+  it should "give the answer to the challenge - Part 2" in {
+    val res = sut.find3Entries(read("day1.txt"))
+    res shouldBe 65656536
+  }
 
 }
