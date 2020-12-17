5a975ac9898a59b95d2edab0a83e661a8dba02f6
lang <hoanglang@gmail.com>
2020-12-17 12:23:26 +0100

    Day 17 - Part 2

--- src/main/scala/day17/ConwayCubes.scala
+++ src/main/scala/day17/ConwayCubes.scala
@@ -5,6 +5,7 @@ import scala.math._
 object ConwayCubes {
 
   type Cube = List[List[String]]
+  type HCube = List[List[List[String]]]
 
   def countNeighbours(x: Int, y: Int, z: Int, cube: Cube): Int = {
     val height = cube.length - 1
@@ -23,7 +24,6 @@ object ConwayCubes {
     val size = flat.size
     val newZ = List.fill(size)("." * size)
     List.fill((size/2.0).ceil.toInt-1)(newZ) ++ (flat :: List.fill(size/2)(newZ))
-    //List(newZ,flat,newZ)
   }
 
   def biggerCube(cube: Cube): Cube = {
@@ -48,7 +48,6 @@ object ConwayCubes {
         case '.' if occupied == 3 => '#'
         case '#' if Set(2, 3)(occupied) => '#'
         case _ => '.'
-        //case c => c
       }
     }
         val t1 = newCube.grouped(pow(height+1,2).toInt).toList
@@ -78,4 +77,85 @@ object ConwayCubes {
     if (display) displayCube(first)
     (1 to n).foldLeft(first)((cube, _) => playRound(cube, display))
   }
+
+
+
+  def countHNeighbours(x: Int, y: Int, z: Int, w: Int, cube: HCube): Int = {
+    val height = cube.length - 1
+    val neighbours = for {
+      l <- max(w - 1, 0) to min(w + 1, height)
+      k <- max(z - 1, 0) to min(z + 1, height)
+      j <- max(y - 1, 0) to min(y + 1, height)
+      i <- max(x - 1, 0) to min(x + 1, height)
+      if i != x || j != y || k != z || l != w
+      if cube(l)(k)(j)(i) == '#'
+    } yield (i, j, k, l)
+    //println(neighbours.mkString(", "))
+    neighbours.size
+  }
+
+  def biggerHCube(cube: HCube): HCube = {
+    val size = cube.size + 2
+    val newW = List.fill(size)(List.fill(size)("." * size))
+    val existing = cube.map(xyz => biggerCube(xyz))
+    (newW :: existing) :+ newW
+  }
+
+  def from2dTo4d(flat: List[String]): HCube = {
+    val size = flat.size
+    val newW = List.fill(size)(List.fill(size)("." * size))
+    List.fill((size/2.0).ceil.toInt-1)(newW) ++ (from2dTo3d(flat) :: List.fill(size/2)(newW))
+  }
+
+  def playHRound(prevCube: HCube, display: Boolean= false): HCube = {
+    val cube = biggerHCube(prevCube)
+
+    val height = cube.length - 1
+    val newCube = for {
+      l <- 0 to height
+      k <- 0 to height
+      j <- 0 to height
+      i <- 0 to height
+    } yield {
+      val occupied = countHNeighbours(i, j, k, l, cube)
+      cube(l)(k)(j)(i) match {
+        case '.' if occupied == 3 => '#'
+        case '#' if Set(2, 3)(occupied) => '#'
+        case _ => '.'
+      }
+    }
+    val t1 = newCube.grouped(pow(height + 1, 3).toInt).toList
+    val t2: List[List[IndexedSeq[Char]]] = t1.map(_.grouped(pow(height + 1, 2).toInt).toList)
+    val res: List[List[List[String]]] = t2.map(_.map(_.grouped(height + 1).map(_.mkString).toList))
+
+    if (display) displayHCube(res)
+    res
+  }
+
+  def countHActives(cube: HCube): Int =
+    cube.flatten.flatten.flatten.count(_ == '#')
+
+  def runHRounds(input:List[String], n: Int, display: Boolean = false): HCube = {
+    val first = from2dTo4d(input)
+    if (display) displayHCube(first)
+    (1 to n).foldLeft(first)((cube, _) => playHRound(cube, display))
+  }
+
+  def displayHCube(cube: HCube): Unit = {
+    println
+    cube.zipWithIndex
+      .foreach{case(xyz, w) =>
+        println(s"=== w=$w ==============")
+        xyz.zipWithIndex.foreach{case(xy, z) =>
+          println(s"== z=$z, w=$w ===")
+          xy.foreach(println)
+          println
+        }
+        println
+      }
+    println
+    println
+  }
+
+
 }
--- src/test/scala/day17/ConwayCubesTest.scala
+++ src/test/scala/day17/ConwayCubesTest.scala
@@ -92,4 +92,35 @@ class ConwayCubesTest extends BaseTestSuite {
     val res = runRounds(input, 6)
     countActives(res) shouldBe 215
   }
+
+  it should "build a bigger hypercube" in {
+    val flatRes = biggerHCube(from2dTo4d(example))
+    flatRes should have size 5
+    flatRes.head should have size 5
+    flatRes.head.head should have size 5
+    flatRes(2)(2)(3)(3) shouldBe '#'
+    //displayHCube(flatRes)
+
+    val res8 = biggerHCube(from2dTo4d(input))
+    res8 should have size 10
+    res8.head should have size 10
+    res8.head.head should have size 10
+    displayHCube(res8)
+  }
+
+  it should "play Hround (example)" in {
+    val res = playHRound(from2dTo4d(example), display = true)
+    val round2 = playHRound(res, display = true)
+    //val round3 = playRound(round2, display = true)
+  }
+
+  it should "count actives cells in Hypercube (example)" in {
+    val res = runHRounds(example, 6, display = true)
+    countHActives(res) shouldBe 848
+  }
+
+  it should "count actives cells in Hypercube - Part 2" in {
+    val res = runHRounds(input, 6, display = false)
+    countHActives(res) shouldBe 1728
+  }
 }
